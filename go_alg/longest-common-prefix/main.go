d3fd614c0f1797deff40e0e30ee240c141f480a9
piftai <piftaiwork@gmail.com>
2024-09-05 01:51:34 +0300

    ...

--- go_alg/longest-common-prefix/main.go
+++ go_alg/longest-common-prefix/main.go
@@ -6,7 +6,7 @@ func main() {
 	str := []string{"a", "a", "a", "aabam"}
 	str1 := []string{"floor", "flower", "flight"}
 	str2 := []string{"ab", "ab", "ab"}
-	fmt.Printf("TEST CASE 1: %s\n", longestCommonPrefix(str))  // correst answer "a"
+	fmt.Printf("TEST CASE 1: %s\n", longestCommonPrefix(str))  // correct answer "a"
 	fmt.Printf("TEST CASE 2: %s\n", longestCommonPrefix(str1)) // correct answer "fl"
 	fmt.Printf("TEST CASE 3: %s\n", longestCommonPrefix(str2)) // correct answer "ab"
 
@@ -14,9 +14,21 @@ func main() {
 
 func longestCommonPrefix(strs []string) string {
 	prefix := strs[0]
+	var cnt int
+	if len(prefix) == 1 {
+		for _, val := range strs {
+			if string(val[0]) == prefix {
+				cnt++
+			}
+		}
+	}
+	if cnt == len(strs) {
+		return prefix
+	}
 	for i := len(prefix) - 1; i >= 0; i-- { // делаем массив, который будет итерироваться по срезу prefix
 		prefix = prefix[:i]
-		cnt := 0
+		fmt.Printf("i: %d, prefix: %s\n", i, prefix)
+		cnt = 0
 		for _, v := range strs { // массив, который сверяет срез каждого элемента strs со срезом prefix, равны ли они друг другу
 			if len(v) >= i && v[:i] == prefix { // чтобы не было out of range - мы должны
 				// проверять есть ли в элементе столько же или больше символов, чем в префиксе
