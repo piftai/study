590a064bfb9f7c3764c44a28107f26f4ecc79530
piftai <piftaiwork@gmail.com>
2024-10-21 01:21:07 +0300

    solved leetcode

--- go_alg/Kids-With-the-Greatest-Number-of-Candies/main.go
+++ go_alg/Kids-With-the-Greatest-Number-of-Candies/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	var array = []int{2, 3, 5, 1, 3}
+
 	var exCandies int = 3
 	fmt.Printf("TEST 1: %v\n", kidsWithCandies(array, exCandies))
 }
@@ -18,9 +19,10 @@ func kidsWithCandies(candies []int, extraCandies int) []bool {
 	res := make([]bool, 0, len(candies)+1)
 	for _, v := range candies {
 		if v+extraCandies >= maxCandy {
-			fmt.Printf("%v because v+extraCandies =")
+			fmt.Printf("%v because v = %v extraCandies = %v\n", true, v, extraCandies)
 			res = append(res, true)
 		} else {
+			fmt.Printf("%v because v = %v extraCandies = %v\n", false, v, extraCandies)
 			res = append(res, false)
 		}
 	}
