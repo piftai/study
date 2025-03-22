2ebfce5cb5a4b57e9709effdcd9cc615a85143c1
piftai <piftaiwork@gmail.com>
2025-03-22 15:52:25 +0300

    repeating basic of lang

--- go_alg/valid-parentheses/main.go
+++ go_alg/valid-parentheses/main.go
@@ -4,10 +4,21 @@ import "fmt" // Print tests
 
 func main() {
 	fmt.Printf("TEST CASE 1: %v\n", isValid("()"))
-	fmt.Printf("TEST CASE 2: %v\n", isValid("()[]{}"))
+	fmt.Printf("TEST CASE 2: %v\n", isValid("12"))
 	fmt.Printf("TEST CASE 3: %v\n", isValid("(]"))
-	fmt.Printf("TEST CASE 4: %v\n", isValid("([])"))
-	fmt.Printf("TEST CASE 5: %v", isValid("([]})"))
+	fmt.Printf("TEST CASE 4: %v\n", isValid(" "))
+	fmt.Printf("TEST CASE 5: %v\n", isValid("\n"))
+	fmt.Printf("TEST CASE 6: %v\n", isValid(`0`))
+	fmt.Printf("TEST CASE 7: %v\n", isValid(`127`))
+	a := ""
+	for i := 0; i < 5; i++ {
+		_, err := fmt.Scan(&a)
+		if err != nil {
+			fmt.Printf("Incorrect data in test case №%d", 8+i)
+		} else {
+			fmt.Printf("TEST CASE %d: %v\n", 8+i, isValid(a))
+		}
+	}
 }
 
 func isValid(s string) bool {
--- yandex_cup_15.11/task_a1.go
+++ yandex_cup_15.11/task_a1.go
@@ -15,15 +15,19 @@ func main() {
 	if err != nil {
 		return
 	}
+	if float64(n) > math.Pow(10, 18) || float64(a) > math.Pow(10, 6) || float64(b) > math.Pow(10, 6) || float64(c) > math.Pow(10, 6) {
+		return
+	}
 	list := make([]uint64, 0, n)
 	for i := uint64(1); float64(i) < math.Pow(10, 18); i++ {
 		if i%a == 0 && i%b == 0 && i%c != 0 || i%a == 0 && i%c == 0 && i%b != 0 || i%b == 0 && i%c == 0 && i%a != 0 {
 			list = append(list, i)
-			if uint64(len(list)) > n && uint64(len(list)) < n+2 {
+			if uint64(len(list)) >= n { // Добавляем проверку длины слайса
 				fmt.Printf("%d", list[n-1])
 				return
 			}
 		}
 	}
 	fmt.Print("-1")
+	return
 }
